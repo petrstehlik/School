8b252095e6bf52cde586933360038275ddb91b0a
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2016-12-22 13:07:26 +0100

    BMS: proj2: first working version
    * Fixed SDTs merging
    * Add PMT parsing and matching with PAT
    * program bitrate calculator

--- BMS/proj2/bms2.py
+++ BMS/proj2/bms2.py
@@ -10,21 +10,23 @@
 """
 import sys
 import struct
-import Tkinter
-import tkMessageBox
-import tkFileDialog
+#import Tkinter
+#import tkMessageBox
+#import tkFileDialog
 import sys
-from optparse import OptionParser
+#from optparse import OptionParser
 from cStringIO import StringIO
-import pdb
+#import pdb
+from os.path import basename, splitext
 
-from nit import printNIT, parseNITSection
+from nit import printNIT, parseNITSection, calcBitrate
 
 programs = dict()
 packets = dict()
 SDTpacket = ""
 NIT = {"network_id" : 0}
-zero = 0
+PAT = dict()
+packetCount = 0
 
 class SystemClock:
 	def __init__(self):
@@ -171,9 +173,7 @@ def parsePESHeader(filehandle, startPos,PESPktInfo):
 		PESPktInfo.setAUType(auType)
 
 
-PAT = dict()
-
-def parsePATSection(filehandle, k, p=False):
+def parsePATSection(filehandle, k):
 
 	local = readFile(filehandle,k,4)
 	table_id = (local>>24)
@@ -197,57 +197,50 @@ def parsePATSection(filehandle, k, p=False):
 		local = readFile(filehandle, j, 4)
 		program_number = (local>>16)
 		program_map_PID = local&0x1FFF
-			#if p: print 'program_map_PID = 0x%04X' %program_map_PID
 
-		if program_number != 0 and hex(program_map_PID) not in NIT:
-			PAT["0x{:04X}".format(program_map_PID)] = {"service_provider": "", "service_name" : "", "program_number" : hex(program_number)}
+		if program_number != 0 and hex(program_map_PID) not in PAT:
+			PAT[hex(program_map_PID)] = {
+					"service_provider": "",
+					"service_name" : "",
+					"program_number" : hex(program_number),
+					"pmt" : []
+				}
 
 		length = length - 4;
 		j += 4
 
 
-def parsePMTSection(filehandle, k):
+def parsePMTSection(filehandle, k, PID):
 
 	local = readFile(filehandle,k,4)
 
 	table_id = (local>>24)
 	if (table_id != 0x2):
-		print 'Ooops! error in parsePMTSection()!'
-		return
-
-	print '------- PMT Information -------'
+		raise Exception('table_id doesn\'t match 0x2 (' + hex(table_id) +')!')
 
 	section_length = (local>>8)&0xFFF
-	print 'section_length = %d' %section_length
 
 	program_number = (local&0xFF) << 8;
 
 	local = readFile(filehandle, k+4, 4)
 
 	program_number += (local>>24)&0xFF
-	print 'program_number = %d' %program_number
 
 	version_number = (local>>17)&0x1F
 	current_next_indicator = (local>>16)&0x1
 	section_number = (local>>8)&0xFF
 	last_section_number = local&0xFF;
-	print 'section_number = %d, last_section_number = %d' %(section_number, last_section_number)
 
 	local = readFile(filehandle, k+8, 4)
 
 	PCR_PID = (local>>16)&0x1FFF
-	print 'PCR_PID = 0x%X' %PCR_PID
 	program_info_length = (local&0xFFF)
-	print 'program_info_length = %d' %program_info_length
 
 	n = program_info_length
 	m = k + 12;
 	while (n>0):
 		descriptor_tag = readFile(filehandle, m, 1)
-		if descriptor_tag == 64:
-			found = True
 		descriptor_length = readFile(filehandle, m+1, 1)
-		print 'descriptor_tag = %d, descriptor_length = %d' %(descriptor_tag, descriptor_length)
 		n -= descriptor_length + 2
 		m += descriptor_length + 2
 
@@ -262,21 +255,20 @@ def parsePMTSection(filehandle, k):
 		elementary_PID = (local2>>16)&0x1FFF
 		ES_info_length = local2&0xFFF
 
-		print 'stream_type = 0x%X, elementary_PID = 0x%X, ES_info_length = %d' %(stream_type, elementary_PID, ES_info_length)
+		if elementary_PID not in PAT[hex(PID)]["pmt"]:
+			PAT[hex(PID)]["pmt"].append(elementary_PID)
+
 		n = ES_info_length
 		m = j+5;
 		while (n>0):
 			descriptor_tag = readFile(filehandle, m, 1)
 			descriptor_length = readFile(filehandle, m+1, 1)
-			print 'descriptor_tag = %d, descriptor_length = %d' %(descriptor_tag, descriptor_length)
 			n -= descriptor_length + 2
 			m += descriptor_length + 2
-
 
 		j += 5 + ES_info_length
 		length -= 5 + ES_info_length
 
-	print ''
 
 def parseSITSection(filehandle, k):
 	local = readFile(filehandle,k,4)
@@ -336,12 +328,8 @@ def parseSITSection(filehandle, k):
 
 
 def parseSDTSection(rawPacket, k):
-	#pdb.set_trace()
-	#print('----------------SDT packet-----------')
 	global NIT
 	filehandle = StringIO(rawPacket)
-
-	#print(filehandle.read().encode('hex'))
 
 	table_id = readFile(filehandle, k, 1)
 	section_length = (readFile(filehandle, k+1, 2) & 0xFFF) - 12
@@ -351,21 +339,16 @@ def parseSDTSection(rawPacket, k):
 	last_section_number = readFile(filehandle, k+7, 1)
 	orig_network_id = readFile(filehandle, k+8, 2)
 
-	#print("section_length: " + str(section_length))
-
 	try:
 		sec = 0
 		while sec < section_length:
 			service_id = readFile(filehandle, k + 11 + sec, 2)
-			#print("service_id", hex(service_id))
 
 			desc_loop_len = readFile(filehandle, k + 14 + sec, 2)& 0xFFF
-			#print("desc_loop_len", desc_loop_len)
 
 			i = 0
 			m = k + 14 + 2 + sec
 			while i <= desc_loop_len:
-				#print("i", i)
 				tag = readFile(filehandle, m + i, 1)
 				length = readFile(filehandle, m + i + 1, 1)
 
@@ -373,7 +356,6 @@ def parseSDTSection(rawPacket, k):
 				if tag == 0x48:
 					provider_name = ""
 					provider_name_len = readFile(filehandle,  m + i + 3, 1)
-					#print('provider_name_len', provider_name_len)
 					j = 1
 					while j <= provider_name_len:
 						provider_name += chr(readFile(filehandle, m + i + 3 + j, 1))
@@ -387,43 +369,32 @@ def parseSDTSection(rawPacket, k):
 						service_name += chr(readFile(filehandle, m + 4 + i + j + provider_name_len, 1))
 						j += 1
 
-
-					#print(provider_name)
-					#print(service_name)
-
 					for key in PAT:
-						print "prog num", PAT[key]["program_number"]
-						print "service_id", hex(service_id)
 						if hex(service_id) == PAT[key]["program_number"]:
-							print("match!")
 							PAT[key]["service_name"] = service_name
 							PAT[key]["service_provider"] = provider_name
-							print(PAT)
-
 
 				i += length + 2
 			sec += desc_loop_len + 5
-			#print("sec:", sec)
 	except IOError:
-		print("io error")
-		filehandle.seek(0,0)
-		print(filehandle.read().encode('hex'))
-		filehandle.seek(0,0)
-		print(filehandle.read())
 		return
 
 
 
-	#print("0x{:04X}".format(ts_id))
-def parseTSMain(filehandle, packet_size, mode, pid, psi_mode, searchItem):
+def parseTSMain():
+	filename = sys.argv[1]
+
+	filehandle = open(filename,'rb')
+
 	global SDTpacket, zero
 
 	PCR = SystemClock()
 	PESPktInfo = PESPacketInfo()
 
 	n = 0
-	packetCount = 0
 	rdi_count = 0
+	packet_size = 188
+	global packetCount
 
 	EntryPESPacketNumList = []
 	TPIList = []
@@ -434,7 +405,6 @@ def parseTSMain(filehandle, packet_size, mode, pid, psi_mode, searchItem):
 	last_SameES_packetNo = 0
 	last_EntryTPI = 0
 
-
 	try:
 		while(True):
 			PacketHeader = readFile(filehandle,n,4)
@@ -442,23 +412,19 @@ def parseTSMain(filehandle, packet_size, mode, pid, psi_mode, searchItem):
 			if ((PacketHeader>>24) != 0x47):
 				raise Exception("SyncByte not found!")
 
-			payload_unit_start_indicator = (PacketHeader >> 22) & 0x1
+			payload_unit_start_indicator = (PacketHeader>>22)&0x1
 
-			PID = ((PacketHeader>>8) & 0x1FFF)
-##            if (PID == 0x0):
-##                print 'Found PAT Packet! packet No. %d' %packetCount
-##                print 'payload_unit_start_indicator = %d' %payload_unit_start_indicator
+			PID = (PacketHeader>>8)&0x1FFF
 
 			if PID not in packets:
-				packets[PID] = { "count" : 1, "hex" : hex(PID) }
+				packets[PID] = 0
 			else:
-				packets[PID]["count"] += 1
+				packets[PID] += 1
 
-			adapt_field = ((PacketHeader>>4)&0x3)
+			adapt_field = (PacketHeader>>4)&0x3
 			Adaptation_Field_Length = 0
 
 			if (adapt_field == 0x1) or (adapt_field == 0x3):
-
 				PESstartCode = readFile(filehandle, n+4, 4)
 
 				"""if ((PESstartCode&0xFFFFFF00) == 0x00000100)&(PID == pid)&(payload_unit_start_indicator == 1):
@@ -491,82 +457,45 @@ def parseTSMain(filehandle, packet_size, mode, pid, psi_mode, searchItem):
 """
 				pointer_field = (PESstartCode >> 24)
 
-				#if ((table_id == 0x0)&(PID != 0x0)):
-				#		print(table_id)
-				#		print 'Something wrong in packet No. %d' %packetCount
-
+				# Adapt Field has length
 				if (adapt_field == 0x3):
 					Adaptation_Field_Length = readFile(filehandle, n + 4, 1)
 
-				k = n+Adaptation_Field_Length+4+1+pointer_field
+				k = n + Adaptation_Field_Length + 4 + 1 + pointer_field
+
 				table_id = readFile(filehandle, k, 1)
 
 				# PAT packet
 				if (PID == 0x0):
-					#print 'pasing PAT Packet! packet No. %d, PID = 0x%X' %(packetCount, PID)
-					parsePATSection(filehandle, k, p=True)
+					parsePATSection(filehandle, k)
 
 				# NIT packet
 				elif (PID == 0x10):
-					#print("found NIT packet")
 					parseNITSection(filehandle, k)
 
-				# SDT
+				# SDT packet
 				elif PID == 0x11:
-					#pdb.set_trace()
 					if payload_unit_start_indicator == 1:
 						if SDTpacket != "":
 							parseSDTSection(SDTpacket, 0)
 						filehandle.seek(k,0)
-						SDTpacket = filehandle.read(184)
+						SDTpacket = filehandle.read(183)
 					else:
-						filehandle.seek(k - pointer_field,0)
+						filehandle.seek(k - pointer_field-1,0)
 						SDTpacket += filehandle.read(184)
 
-
 				# PMT packet
-				"""elif (table_id == 0x2):
-					if ((psi_mode == 2) & (searchItem == "PMT")):
-						if PID not in PIDList:
-							PIDList.append(PID)
-						else:
-							n += packet_size
-							packetCount += 1
-							continue
-					#print 'pasing PMT Packet! packet No. %d, PID = 0x%X' %(packetCount, PID)
-					#parsePMTSection(filehandle, k)
+				elif (table_id == 0x2) and hex(PID) in PAT:
+					parsePMTSection(filehandle, k, PID)
 
-				# SIT packet
-				elif (table_id == 0x7F):
-					if ((psi_mode == 2)&(searchItem == "SIT")):
-						if PID not in PIDList:
-							PIDList.append(PID)
-						else:
-							n += packet_size
-							packetCount += 1
-							continue
-					#print 'pasing SIT Packet! packet No. %d, PID = 0x%X' %(packetCount, PID)
-					#parseSITSection(filehandle, k)
-
-				if (PID == pid):
-					last_SameES_packetNo = packetCount
-					"""
-
-			# skip to next TS packet and increase packet count by 1.
+			# seek to next packet and increment packet count
 			n += packet_size
 			packetCount += 1
 
 	except IOError:
-		print 'Finished reading TS file'
-		print(PIDList)
-		print 'Packet count: %S', packetCount
 		filehandle.close()
 	else:
 		filehandle.close()
-
-	print '================================================\n'
-	for i in range(len(EntryPESPacketNumList)):
-		print 'TPI = 0x%x, PTS = 0x%x, EntryPESPacketNum = 0x%x' %(TPIList[i], PTSList[i], EntryPESPacketNumList[i])
 
 
 def getFilename():
@@ -575,88 +504,54 @@ def getFilename():
 	iDir='~/'
 	filename=tkFileDialog.askopenfilename(filetypes=fTyp,initialdir=iDir)
 	root.destroy()
-	return filename;
+	return filename
 
-def Main():
+def calcProgramBitrate(pids, bitrate):
+	prog_packets = 0
+	for pid in pids:
+		try:
+			prog_packets += packets[pid]
+		except:
+			pass
 
-	description = "This is a python script for parsing MPEG-2 TS stream"
-	usage = "\n\t%prog -t <188|192|204> -m PAT\
-			\n\t%prog -t <188|192|204> -m <PMT|ES|SIT> PID\
-			\n\t%prog -s PCR \
-			\n\t%prog -s <PAT|PMT|SIT> --all \
-			\n\t%prog -s <PAT|PMT|SIT> --unique\n\n \
-			Example: TSParser.py -t 188 -m PMT 1fc8"
+	res = (float(prog_packets) / packetCount) * bitrate
+	return res
 
-	cml_parser = OptionParser(description = description, usage=usage)
-	cml_parser.add_option("-f", "--file", action="store", type="string", dest="filename", default="",
-			help="specify file name, if not specified, a file open dialogbox will be shown.")
+def printPAT():
+	tmp_PAT = dict()
+	bitrate = calcBitrate()
 
-	cml_parser.add_option("-t", "--type", action="store", type="int", dest="packet_size", default="188",
-			help="specify TS packet size[188, 192, 204], default = 188")
+	#reorder keys
+	for key, value in PAT.iteritems():
+		tmp_PAT["0x{:04x}".format(int(key, 16))] = value
 
-	cml_parser.add_option("-m", "--mode", action="store", type="string", dest="mode", default="PAT",
-			help="specify parsing mode[PAT, PMT, SIT, ES], default = PAT")
-
-	cml_parser.add_option("-s", "--search", action="store", type="string", dest="searchItem", default="FFF",
-			help="search PAT/PMT/PCR/SIT packets and output Information.")
-
-	cml_parser.add_option("--all", action="store_const", const=10, dest="psi_mode", default=0,
-			help="Output all PAT/PMT/SIT packets Information. default, only the first one is output.")
-
-	cml_parser.add_option("--unique", action="store_const", const=2, dest="psi_mode", default=0,
-			help="Output unique PAT/PMT/SIT packets Information. default, only the first one is output.")
-
-	(opts, args) = cml_parser.parse_args(sys.argv)
-
-	if ((opts.searchItem == "FFF") & (opts.mode != "PAT") & (len(args) < 2)):
-		cml_parser.print_help()
-		return
-
-	if ((opts.searchItem == "FFF") & (opts.mode != "PAT")):
-		pid = int(args[1], 16)
-	else:
-		pid = 0;
-
-	if ((opts.searchItem != "FFF") & (opts.searchItem != "PAT") & \
-			(opts.searchItem != "PMT") & (opts.searchItem != "PCR") &
-			(opts.searchItem != "SIT")):
-		cml_parser.print_help()
-		return
-
-	psi_mode = 0
-	if (opts.searchItem != "FFF"):
-		psi_mode = opts.psi_mode
-
-	if (opts.filename == ""):
-		filename = getFilename()
-	else:
-		filename = opts.filename
-
-	if (filename == ""):
-		return
-
-	print filename
-	filehandle = open(filename,'rb')
-
-	parseTSMain(filehandle, opts.packet_size, opts.mode, pid, psi_mode, opts.searchItem)
+	for key, value in sorted(tmp_PAT.iteritems()):
+		pids = value['pmt']
+		pids.append(int(key, 16))
+		print "%s-%s-%s: %.2f Mbps" % (key, value['service_provider'], value['service_name'], calcProgramBitrate(pids, bitrate))
 
 
 if __name__ == "__main__":
-	global NIT
+	if len(sys.argv) != 2:
+		raise Exception("You can specify only filename to read from!")
 
-	Main()
-	#print(programs)
-	#print(packets)
+	fileout = splitext(sys.argv[1])[0] + '.txt'
 
-	#for pid in packets:
-#		if pid in programs:
-#			print("\t\t" + hex(pid))
-#			print(packets[pid])
-#			print(programs[pid])
+	outhandle = open(fileout, 'w')
+	sys.stdout = outhandle
+
+	parseTSMain()
+	bandwidth = calcBitrate()
 
 	printNIT()
+	printPAT()
 
-	print PAT
+	outhandle.close()
 
-	for key, value in sorted(PAT.iteritems()):
-		print "%s: %s" % (key, value)
+	"""for key, value in sorted(PAT.iteritems()):
+		print "%s (%d): %s" % (key, int(key, 16), value)
+
+	for key, value in sorted(packets.iteritems()):
+		print "%s: %s" % (key, value)"""
+
+
