0ab122e38dee01ae963f9ea66b37eeb2c37c9767
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-03-10 16:01:20 +0100

    Advance to -1 param

--- FLP/simplify-bkg/src/parsefile.hs
+++ FLP/simplify-bkg/src/parsefile.hs
@@ -33,22 +33,6 @@ data CFG = CFG { nonTerminals :: [String]
 				, rules :: [Rule]
 			} deriving (Eq, Show)
 
-data Params = Params {
-	first_alg :: Bool
-		, second_alg :: Bool
-		, printFlag :: Bool
-		, inputFile :: String
-		, outputFile :: String
-} deriving (Show, Eq)
-
-defaultParams = Params {
-	first_alg    = False
-	, second_alg = False
-	, printFlag = False
-	, inputFile  = ""
-	, outputFile = ""
-}
-
 main :: IO ()
 main = do
 	args <- getArgs
@@ -61,38 +45,68 @@ main = do
 			then saveCFG cfg (outputFile params)
 		else
 			printCFG cfg
-	else print "should continue"
+	else if first_alg params then do
+		let nTerm = (termGenerators cfg)
 
-	print cfg
-
-	print "done"
-
-parseArgs :: [[Char]] -> Params
-parseArgs ["-i"] = defaultParams { printFlag = True }
-parseArgs ["-i", inFile] = defaultParams {
-	printFlag = True
-	, inputFile = inFile
-}
-parseArgs ["-i", inFile, outFile] = defaultParams {
-	printFlag = True
-	, inputFile = inFile
-	, outputFile = outFile
-}
-
-parseArgs ["-1"] = defaultParams { first_alg = True }
---parseArgs ["-2"] = (False, True, False, "", "")
-
---parseArgs ["-i"] = (False, False, True, "", "")
---parseArgs ["-i", inFile] = (False, False, True, inFile, "")
---parseArgs ["-i", inFile, outFile] = (False, False, True, inFile, outFile)
-parseArgs [] = error "Flag must be set"
-parseArgs [_] = error "Flag must be set"
+		printCFG CFG {
+			nonTerminals = ( nTerm)
+			, startSymbol = (startSymbol cfg)
+			, terminals = (terminals cfg)
+			, rules = (clearRules nTerm (rules cfg) [])
+		}
+	else
+		print "shold go all the way"
 
 fetchFile :: String -> IO String
 fetchFile inFile = if length inFile  > 0
 	then readFile inFile
 	else getContents
 
+termGenerators :: CFG -> [String]
+termGenerators cfg = ( termGenerators' (rules cfg) [[startSymbol cfg]] )
+
+termGenerators' :: [Rule] -> [String] -> [String]
+termGenerators' rules nonTerms = do
+	let newNonTerms = (termGenerators'' rules nonTerms) --`debug` "new: " ++ newNonTerms
+	nub $ until ( /= newNonTerms) (termGenerators'' rules) newNonTerms
+
+termGenerators'' :: [Rule] -> [String] -> [String]
+termGenerators'' (rule : rules) nonTerms  = if (isTermGenerator rule nonTerms)
+	then termGenerators'' rules (nonTerms ++ [[nt rule]])
+	else termGenerators'' rules nonTerms
+termGenerators'' [] nonTerms = nonTerms
+
+isTermGenerator :: Rule -> [String] -> Bool
+isTermGenerator rule nonTerms = if ( any isLower (body rule) ) &&
+	(termGenerator (body rule) nonTerms)
+		then True
+	else if ( (body rule) == "#" )
+		then True
+	else False --`debug` (body rule)
+
+termGenerator :: String -> [String] -> Bool
+termGenerator (nonT : bodyRule ) nonTerms = if ([[nonT]] \\ nonTerms == [])
+		then termGenerator bodyRule nonTerms
+	else if (isLower nonT)
+		then termGenerator bodyRule nonTerms
+	else False
+termGenerator [] _ = True
+
+clearRules :: [String] -> [Rule] -> [Rule] -> [Rule]
+clearRules nonTerms (rule : rules) clr = do
+	let clearedNonTerm = ([[nt rule]] \\ nonTerms)
+	if clearedNonTerm == [] &&
+	   (all isLower (stripChars (concat nonTerms) (body rule)))
+	then clearRules nonTerms rules (clr ++ [Rule {nt = (nt rule), body = (body rule)}])
+	else if clearedNonTerm == [] &&
+		length (stripChars (concat nonTerms) (body rule)) == 0
+	then clearRules nonTerms rules (clr ++ [Rule {nt = (nt rule), body = (body rule)}])
+	else clearRules nonTerms rules clr
+
+clearRules nonTerms [] clr = clr
+--
+-- Parse input CFG
+--
 parseContent :: [String] -> CFG
 parseContent content = if (null content || length content < 4)
 	then error "Wrong input file"
@@ -102,7 +116,6 @@ parseContent content = if (null content || length content < 4)
 		, terminals = parseTerminals content
 		, rules = parseRules content
 	}
-	--else [getStartState content]
 
 parseNonTerminals :: [String] -> [String]
 parseNonTerminals (nonTerm : _ ) = splitOn "," nonTerm
@@ -124,22 +137,14 @@ parseRule :: String -> Rule
 parseRule (rule) = parseRule' (splitOn "->" rule)
 
 parseRule' :: [String] -> Rule
-parseRule' (nonterm : rule) = Rule {
-									nt = nonterm !! 0
-									, body = rule !! 0
-								}
+parseRule' (nonterm : rule) = Rule { nt = nonterm !! 0, body = rule !! 0 }
 
-splitAtComma :: String -> [String]
-splitAtComma ""  = []
-splitAtComma seq = let (s, seq') = break (== ',') seq
-		   in s : case seq' of
-			       []	   -> []
-			       (_ : seq'') -> splitAtComma seq''
-
-
+--
+-- print and save the CFG
+--
 printCFG :: CFG -> IO ()
 printCFG cfg = do
-	putStrLn (intercalate "," ( nonTerminals cfg ) )
+	putStrLn (intercalate "," ( ( nonTerminals cfg )) )
 	putStrLn (intercalate "," ( terminals cfg ) )
 	putStrLn ([startSymbol cfg])
 	putStrLn (intercalate "\n" ( map ( ruleToString ) ( rules cfg ) ) )
@@ -150,8 +155,6 @@ saveCFG cfg outFile = do
 	appendFile outFile ( (intercalate "," ( terminals cfg ) ) ++ "\n" )
 	appendFile outFile ( ( [startSymbol cfg] ) ++ "\n" )
 	appendFile outFile ((intercalate "\n" ( map ( ruleToString ) ( rules cfg ) ) )++"\n")
-
---rulesToString :: [Rule] -> [String]
 
 ruleToString :: Rule -> String
 ruleToString rule = [(nt rule)] ++ "->" ++ (body rule)
