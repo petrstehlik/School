1e3e5c19f7cbaf53fe2228e70ff4a5b34927ccf4
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-03-21 20:28:19 +0100

    PRL #2: add detection of duplicates

--- PRL/proj2/es.cpp
+++ PRL/proj2/es.cpp
@@ -26,6 +26,7 @@ int main(int argc, char **argv)
 
     cpu_id = COMM_WORLD.Get_rank();
     cpu_num = COMM_WORLD.Get_size();
+        int dup = 0;
 
     // CPU with ID 0 loads the numbers file
     if (cpu_id == 0) {
@@ -55,6 +56,7 @@ int main(int argc, char **argv)
         COMM_WORLD.Recv(&reg.X, 1, MPI_INT, MASTER_CPU, REG_X, status);
         int duplicate = 0;
 
+
         for (int i = 1; i < cpu_num; i++) {
             // Receive the y_i-1 value to the register Y
             COMM_WORLD.Recv(&reg.Y, 1, MPI_INT, cpu_id - 1, REG_Y, status);
@@ -62,19 +64,35 @@ int main(int argc, char **argv)
             if (reg.X != REG_EMPTY && reg.Y != REG_EMPTY) {
                 if (reg.X > reg.Y)
                     reg.C++;
+
+                if (reg.X == reg.Y)
+                    dup++;
             }
 
             // Send Y register value to my neighbour
             if (cpu_id != cpu_num - 1)
                 COMM_WORLD.Send(&reg.Y, 1, MPI_INT, cpu_id + 1, REG_Y);
-
         }
+
+        /*for (int i = cpu_id; i < cpu_num; i++) {
+            // Receive the y_i-1 value to the register Y
+            //COMM_WORLD.Recv(&reg.Y, 1, MPI_INT, cpu_id - 1, REG_Y, status);
+
+            if (reg.X != REG_EMPTY && reg.Y != REG_EMPTY) {
+                if (reg.X > reg.Y)
+                    reg.C++;
+            }
+
+            // Send Y register value to my neighbour
+            //if (cpu_id != cpu_num - 1)
+                //COMM_WORLD.Send(&reg.Y, 1, MPI_INT, cpu_id + 1, REG_Y);
+        }*/
     }
 
     MPI_Barrier(COMM_WORLD);
 
-    //cout << cpu_id << ": REG: " << reg.C << " / " << reg.X << " / " << reg.Y << " / " << reg.Z << endl;
-    //MPI_Barrier(COMM_WORLD);
+    cout << cpu_id << ": REG: " << reg.C << " / " << reg.X << " / " << reg.Y << " / " << reg.Z << "  dup: " << dup << endl;
+    MPI_Barrier(COMM_WORLD);
 
     if (cpu_id != MASTER_CPU) {
         COMM_WORLD.Isend(&reg.X, 1, MPI_INT, reg.C, REG_Z);
