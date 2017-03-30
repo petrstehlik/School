9fec3d05ebfb9fdcb30f8a834a290614ec94c543
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-03-30 14:31:41 +0200

    PRL #2: cosmetic changes and optimizations

--- PRL/proj2/es.cpp
+++ PRL/proj2/es.cpp
@@ -5,7 +5,6 @@
  */
 
 #include "es.h"
-#include <mpi.h>
 
 using namespace std;
 
@@ -15,23 +14,20 @@ int main(int argc, char **argv)
 {
     int cpu_id;     // ID of the current CPU
     int cpu_num;    // Number of CPUs running
-    MPI::Status status;
+    Status status;  // Status struct for Send and Recv
     TReg reg = initRegisters();
 
     // MPI Initialization
     Init(argc, argv);
 
     // Measure compute time
-    double start = MPI_Wtime();
+    double start = Wtime();
 
+	// Get rank of current CPU
     cpu_id = COMM_WORLD.Get_rank();
-    cpu_num = COMM_WORLD.Get_size();
-    int dup = 0;
 
-    // Array for broadcasting duplicates
-    // 0 - the duplicate value
-    // 1 - the originator
-    int dup_num[2] ={REG_EMPTY, cpu_id};
+    // Get total number of CPUs
+    cpu_num = COMM_WORLD.Get_size();
 
     // CPU with ID 0 loads the numbers file
     if (cpu_id == 0) {
@@ -50,8 +46,11 @@ int main(int argc, char **argv)
                 cout << " " << number;
             }
 
-            MPI::COMM_WORLD.Send(&number, 1, MPI_INT, i+1, REG_X);
-            MPI::COMM_WORLD.Send(&number, 1, MPI_INT, 1, REG_Y);
+			// Send read number to i-th CPU
+            COMM_WORLD.Send(&number, 1, MPI_INT, i+1, REG_X);
+
+            // Send read number and its index to 1st CPU
+            COMM_WORLD.Send(&number, 1, MPI_INT, 1, REG_Y);
             COMM_WORLD.Send(&i, 1, MPI_INT, 1, ORD);
         }
 
@@ -60,75 +59,67 @@ int main(int argc, char **argv)
     } else {
         // Save the x_i number to the register X
         COMM_WORLD.Recv(&reg.X, 1, MPI_INT, MASTER_CPU, REG_X, status);
-        int duplicate = 0;
-        int j = REG_EMPTY;
+        int duplicate = 0;	// How many duplicates is there
+        int j = REG_EMPTY;	// tmp register, holds the Y number's index
 
         for (int i = 1; i < cpu_num; i++) {
-            // Receive the y_i-1 value to the register Y
+            // Receive the y_i-1 value to the register Y...
             COMM_WORLD.Recv(&reg.Y, 1, MPI_INT, cpu_id - 1, REG_Y, status);
-
+			// ...and its index
             COMM_WORLD.Recv(&j, 1, MPI_INT, cpu_id - 1, ORD, status);
 
+			// The comparison algorithm
             if (reg.X != REG_EMPTY && reg.Y != REG_EMPTY) {
+				// Original check
                 if (reg.X > reg.Y)
                     reg.C++;
 
+				// Check duplicities
                 if (reg.X == reg.Y) {
                     dup++;
 
+					// We will always have at least one duplicity
+					// when we compare the same numbers.
+					// Otherwise we will have a real duplicity
                     if (dup > 1) {
-                        if (j >= cpu_id)
+                        if (j < cpu_id)
                             reg.C++;
                     }
                 }
             }
 
-            // Send Y register value to my neighbour
+            // Send Y register value and its index to my neighbour
             if (cpu_id != cpu_num - 1) {
                 COMM_WORLD.Send(&reg.Y, 1, MPI_INT, cpu_id + 1, REG_Y);
                 COMM_WORLD.Send(&j, 1, MPI_INT, cpu_id + 1, ORD);
             }
         }
-
-        /*for (int i = cpu_id; i < cpu_num; i++) {
-            // Receive the y_i-1 value to the register Y
-            //COMM_WORLD.Recv(&reg.Y, 1, MPI_INT, cpu_id - 1, REG_Y, status);
-
-            if (reg.X != REG_EMPTY && reg.Y != REG_EMPTY) {
-                if (reg.X > reg.Y)
-                    reg.C++;
-            }
-
-            // Send Y register value to my neighbour
-            //if (cpu_id != cpu_num - 1)
-                //COMM_WORLD.Send(&reg.Y, 1, MPI_INT, cpu_id + 1, REG_Y);
-        }*/
     }
 
+	// We must wait for all CPUs to finish comparisons
     MPI_Barrier(COMM_WORLD);
 
-    //cout << cpu_id << ": REG: " << reg.C << " / " << reg.X << " / " << reg.Y << " / " << reg.Z << "  dup: " << dup << endl;
-    MPI_Barrier(COMM_WORLD);
-
+	// Set register Z values (async)
     if (cpu_id != MASTER_CPU) {
         COMM_WORLD.Isend(&reg.X, 1, MPI_INT, reg.C, REG_Z);
-
-        //COMM_WORLD.Bcast(&to_send, 1, MPI_INT, i);
         COMM_WORLD.Irecv(&reg.Z, 1, MPI_INT, MPI_ANY_SOURCE, REG_Z);
     }
 
+	// Everyone has the correct number now
     MPI_Barrier(COMM_WORLD);
 
-   for (int i = 1; i < cpu_num; i++) {
-        // Master CPU will print the number
+	for (int i = 1; i < cpu_num; i++) {
+		// Master CPU will print the number
         if (cpu_id == MASTER_CPU) {
             COMM_WORLD.Recv(&reg.Z, 1, MPI_INT, 1, REG_Z);
             cout << reg.Z << endl;
         } else {
+        	// Shift the z number to lower neighbour
             COMM_WORLD.Send(&reg.Z, 1, MPI_INT, cpu_id - 1, REG_Z);
         }
 
         if (cpu_id != MASTER_CPU && cpu_id != cpu_num - 1) {
+        	// Accept the shifting
             COMM_WORLD.Recv(&reg.Z, 1, MPI_INT, cpu_id + 1, REG_Z);
         }
     }
