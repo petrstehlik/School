a1b858912a496aafc71cfd04f879a2b674dbb628
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-04-25 10:20:47 +0200

    POS #2: pidlist finishing

--- POS/proj2/pidlist.c
+++ POS/proj2/pidlist.c
@@ -7,19 +7,22 @@ void pidlist_init(pidlist_t *p) {
 
 void pidlist_insert(pidlist_t *p, int pid) {
 	piditem_t *i;
-	i = malloc(i, sizeof(piditem_t));
+	i = malloc(sizeof(piditem_t));
 	i->pid = pid;
 	i->next = NULL;
 
+    /**
+      * First insert into list
+      */
 	if (p->size == 0) {
 		p->head = i;
 	} else {
 		piditem_t *n;
 		n = malloc(sizeof(piditem_t));
-		n->next = p->head;
+		n = p->head;
 
 		while(n->next != NULL)
-			n->next = n->next->next;
+			n = n->next;
 
 		n->next = i;
 	}
@@ -36,6 +39,8 @@ int pidlist_find(pidlist_t *p, int pid) {
 	while(i != NULL) {
 		if (i->pid == pid)
 			return 1;
+
+		i = i->next;
 	}
 
 	return 0;
@@ -47,20 +52,39 @@ int pidlist_remove(pidlist_t *p, int pid) {
 
 	i = p->head;
 
+	if (i == NULL)
+	    return 0;
+
 	if (i->pid == pid) {
 		p->head = i->next;
 		free(i);
+		p->size--;
 		return 1;
 	}
 
 	while(i != NULL) {
-		if (i->next != NULL)
-		if (i->pid == pid) {
-
-			return 1;
+		if (i->next != NULL) {
+    		if (i->next->pid == pid) {
+    		    i->next = i->next->next;
+                free(i);
+                p->size--;
+			    return 1;
+            }
 		}
 	}
 
+    free(i);
 	return 0;
+}
 
+void pidlist_print(pidlist_t *p) {
+    piditem_t *i;
+    i = malloc(sizeof(piditem_t));
+
+    i = p->head;
+    printf("Total size: %d\n", p->size);
+    while(i != NULL) {
+        printf("PID: %d\n", i->pid);
+        i = i->next;
+    }
 }
--- POS/proj2/pidlist.h
+++ POS/proj2/pidlist.h
@@ -1,20 +1,37 @@
 #ifndef PIDLIST_H
 #define PIDLIST_H
 
-typedef struct {
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct piditem {
 	int pid;
-	piditem_t *next;
+	struct piditem *next;
 } piditem_t;
 
-typedef struct {
+typedef struct pidlist {
 	piditem_t *head;
 	int size;
 } pidlist_t;
 
 void pidlist_init(pidlist_t *p);
 void pidlist_insert(pidlist_t *p, int pid);
-int pidlist_remove(pidlist_t *p, int pid);
+
+/**
+  * Find a given PID in the list
+  *
+  * \return number of records found (0 or 1)
+  */
 int pidlist_find(pidlist_t *p, int pid);
-void pidlist_free(pidlist_t *p);
+
+/**
+  * Remove a given PID from the list
+  *
+  * \return number of records deleted (0 or 1)
+  */
+int pidlist_remove(pidlist_t *p, int pid);
+/**void pidlist_free(pidlist_t *p);*/
+
+void pidlist_print(pidlist_t *p);
 
 #endif
