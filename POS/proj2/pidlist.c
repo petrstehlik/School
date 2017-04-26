feb0cebc7536724d0a96d8698d5c9c603a307eb1
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-04-26 12:17:03 +0200

    POS #2: pidlist leakage fixes and _killall func
    Also all is now commented

--- POS/proj2/pidlist.c
+++ POS/proj2/pidlist.c
@@ -1,3 +1,21 @@
+/**
+ * POS project #2: Simple shell implementation in POSIX C using two threads
+ *
+ * Author: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
+ * Date: 2017/04/26
+ *
+ * File: pidlist.c
+ */
+
+#define _XOPEN_SOURCE 501
+#include <stdio.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#include <stdlib.h>
+
 #include "pidlist.h"
 
 void pidlist_init(pidlist_t *p) {
@@ -37,16 +55,19 @@ void pidlist_insert(pidlist_t *p, int pid) {
 
 int pidlist_find(pidlist_t *p, int pid) {
 	piditem_t *i;
-	i = malloc(sizeof(piditem_t));
-
 	i = p->head;
 
 	while(i != NULL) {
-		if (i->pid == pid)
+		if (i->pid == pid) {
+			i = NULL;
 			return 1;
+		}
 
 		i = i->next;
 	}
+
+	i = NULL;
+	free(i);
 
 	return 0;
 }
@@ -67,6 +88,7 @@ int pidlist_remove(pidlist_t *p, int pid) {
 				prev->next = i->next;
 
             free(i);
+            prev = NULL;
             p->size--;
 			return 1;
 		}
@@ -74,12 +96,14 @@ int pidlist_remove(pidlist_t *p, int pid) {
 		i = i->next;
 	}
 
+	i = NULL;
+	prev = NULL;
+
 	return 0;
 }
 
 void pidlist_print(pidlist_t *p) {
     piditem_t *i;
-    i = malloc(sizeof(piditem_t));
 
     i = p->head;
     printf("Total size: %d\n", p->size);
@@ -87,4 +111,23 @@ void pidlist_print(pidlist_t *p) {
         printf("PID: %d\n", i->pid);
         i = i->next;
     }
+
+    i = NULL;
+}
+
+void pidlist_killall(pidlist_t *p) {
+	piditem_t *i;
+	i = p->head;
+
+	while (i != NULL) {
+		kill(i->pid, SIGTERM);
+		i = i->next;
+		/** Without this sleep somehow the SIGCHLD is not caught during ending procedure
+		 *
+		 * I know it is nasty and shouldn't be there but you know...
+		 */
+		sleep(1);
+		fflush(stderr);
+	}
+	waitpid(-1, NULL, 0);
 }
--- POS/proj2/pidlist.h
+++ POS/proj2/pidlist.h
@@ -1,20 +1,43 @@
+/**
+ * POS project #2: Simple shell implementation in POSIX C using two threads
+ *
+ * \note: Simple linked list implementation
+ *
+ * Author: Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
+ * Date: 2017/04/26
+ *
+ * File: pidlist.h
+ */
+
 #ifndef PIDLIST_H
 #define PIDLIST_H
 
-#include <stdlib.h>
-#include <stdio.h>
-
+/**
+ * \brief PID item structure with pointer to next item
+ */
 typedef struct piditem {
 	int pid;
 	struct piditem *next;
 } piditem_t;
 
+/**
+ * \brief Holds the linked list and info about its size
+ */
 typedef struct pidlist {
 	piditem_t *head;
 	int size;
 } pidlist_t;
 
+/**
+ * \brief Initialize the pidlist_t structure
+ */
 void pidlist_init(pidlist_t *p);
+
+/**
+ * \brief Insert an item to the end of the list
+ *
+ * \note doesn't check for duplicates
+ */
 void pidlist_insert(pidlist_t *p, int pid);
 
 /**
@@ -30,8 +53,14 @@ int pidlist_find(pidlist_t *p, int pid);
   * \return number of records deleted (0 or 1)
   */
 int pidlist_remove(pidlist_t *p, int pid);
-/**void pidlist_free(pidlist_t *p);*/
 
+/**
+ * \brief print the list
+ */
 void pidlist_print(pidlist_t *p);
 
+/**
+ * \brief Send SIGTERM to all PIDs present in the list
+ */
+void pidlist_killall(pidlist_t *p);
 #endif
