baf222f5621078f954fa3b0de2cc7237e1217041
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-05-07 21:45:41 +0200

    Move functionality to gif class

--- KKO/kko.proj3/gif2bmp.h
+++ KKO/kko.proj3/gif2bmp.h
@@ -1,48 +1,8 @@
-// GCT
-
-#ifndef DEBUG
-#define DEBUG false
-#endif
-
 #pragma once
 
 #include <sys/types.h>
 #include <vector>
 #include <string>
-
-#define IMAGE_DESCRIPTOR	0x2C
-#define TRAILER				0x3B
-#define GRAPHIC_CONTROL		0xF9
-#define APPLICATION_EXT		0xFF
-#define COMMENT_EXT			0xFE
-#define PLAINTEXT_EXT		0x01
-#define MAX_CODE_LENGTH		12
-
-typedef struct {
-	int64_t bmpSize;
-	int64_t gifSize;
-} tGIF2BMP;
-
-#pragma pack(push, 1)
-typedef struct {
-	uint8_t signature[3];
-	uint8_t version[3];
-} gifHeader;
-
-typedef struct {
-	uint16_t width;
-	uint16_t height;
-	uint8_t packed;
-	uint8_t bgColorIndex;
-	uint8_t aspectRatio;
-} descriptor;
-
-typedef struct {
-	uint8_t GCT_flag;
-	uint8_t colorResolution;
-	uint8_t sortFlag;
-	uint8_t GCT_size;
-} packedField;
 
 struct color {
 	uint8_t r;
@@ -51,36 +11,9 @@ struct color {
 };
 
 typedef struct {
-	uint8_t size;
-	uint8_t packed;
-	uint16_t delay;
-	uint8_t transparentColorIdx;
-	uint8_t terminator;
-} GCE;
-
-typedef struct {
-	bool LCT_flag;
-	bool interlace_flag;
-	bool sort_flag;
-	uint8_t futureuse;
-	uint8_t LCT_size;
-} imgDescPack;
-
-typedef struct {
-	uint16_t pos_left;
-	uint16_t pos_top;
-	uint16_t width;
-	uint16_t height;
-	imgDescPack p;
-} imgDescriptor;
-
-#pragma pack(pop)
-
-typedef struct {
-	uint8_t byte;
-	int32_t prev;
-	int len;
-} dictionary_entry_t;
+	int64_t bmpSize;
+	int64_t gifSize;
+} tGIF2BMP;
 
 
 struct GIFError : public std::exception
@@ -100,31 +33,3 @@ struct GIFError : public std::exception
  * @return -1 pri prevodu doslo k chybe, prip. nepodporuje dany format GIF
  */
 int gif2bmp(tGIF2BMP *gif2bmp, FILE *inputFile, FILE *outputFile);
-
-void readHeader(FILE *f, gifHeader *h);
-void readDescriptor (FILE *f, descriptor *d);
-void parsePackedField(descriptor *d, packedField *p);
-void parseImgDescPack(imgDescPack *i, char c);
-int POW(int x);
-
-/**
-  * @str String to to write to stderr
-  * @return error code -1
-  */
-int error(const char *str);
-
-
-/** Parse color table
-  *
-  * The color table can be global or local, it doesn't matter
-  *
-  * @param FILE * inFile input file
-  */
-//void parseColorTable(FILE *inFile, colorTable_t *table);
-
-
-void uncompress( int code_length,
-                const unsigned char *input,
-                int input_length,
-                unsigned char *out );
-
