613be5a3bdc040aac8adb51395456daf6a5fd977
Petr Stehlik <xstehl14@stud.fit.vutbr.cz>
2017-11-13 16:41:59 +0100

    Use forward propagate

--- SFC/network/__main__.py
+++ SFC/network/__main__.py
@@ -2,7 +2,17 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-from Network import Network
+from network import Network
 from neuron import Neuron
 
-network = Network(5, [3,3], 2)
+network = Network(5, [3,3], 1)
+
+res = network.forward_propagate([
+        0.25,
+        0.125,
+        0.265,
+        0.75,
+        0.95
+    ])
+
+print(res)
