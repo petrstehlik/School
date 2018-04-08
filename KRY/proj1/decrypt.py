8909c5eff24f0adc74a2fcb6a33cb3b8cb915a96
Petr Stehlik <petr.stehlik@kiwi.com>
2018-04-08 16:18:28 +0200

    KRY: working decrypt script

--- KRY/proj1/decrypt.py
+++ KRY/proj1/decrypt.py
@@ -1,36 +1,60 @@
-from .cipher import step, N
+#!/usr/bin/python3
 
-key = []
-
-with open('in/bis.txt', "rb") as a:
-    with open('in/bis.txt.enc', "rb") as b:
-        key = [chr(a ^ b) for a,b in zip(list(a.read()), list(b.read()))]
-
-
-with open('in/super_cipher.py.enc', 'rb') as f:
-    content = list(f.read())
-
-    a = [chr(a) for a in content]
-
-    code = []
-
-    for idx, val in enumerate(content):
-        try:
-            res = val ^ ord(key[idx])
-            code.append(chr(res))
-        except:
-            if idx % 32 == 0:
-                print(idx)
-                res = step(int.from_bytes(''.join(key[0:32]).encode(), 'little'))
-
-                for i in range(N):
-                    res = step(res)
-
-                c = []
-                # print(res)
-                print(int.from_bytes(content[idx:idx+32], 'little') ^ res)
+SUB = [0, 1, 1, 0, 1, 0, 1, 0]
+ISUB = {
+    '0': [],
+    '1': [],
+}
+N_B = 32
+N = 8 * N_B
 
 
-print("".join(code))
-# for idx, i in enumerate(key):
-#    print(i, end='')
+def convert_to_bin(x, fill=N):
+    """Convert number to binary representation, stretched to given lenght and reversed."""
+    return bin(x)[2:].zfill(fill)[::-1]
+
+
+def stretch(y):
+    """Stretch from 32 to 34 bytes"""
+    return (y >> 1) & 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+
+
+def rev_next(x):
+    binin = convert_to_bin(x)
+    valid = ['']
+    for i in range(0, len(binin)):
+        bit = binin[i]
+        newvalid = []
+
+        for v in valid:
+            for group in ISUB[bit]:
+                if i > 0 and v[-2:] == group[:2]:
+                    newvalid.append(v[:-2] + group)
+                elif i == 0:
+                    newvalid.append(group)
+        valid = newvalid
+
+    for v in valid:
+        """Return the one which is symmetrical"""
+        if v[:2] == v[-2:]:
+            return int(v[::-1], 2)
+
+    return None
+
+
+if __name__ == '__main__':
+    for bit in [0, 1]:
+        for i in range(len(SUB)):
+            if SUB[i] == bit:
+                ISUB[str(bit)].append(convert_to_bin(i, fill=3))
+
+    # First key stream block
+    y = 76211941219306483308438160659696441778580117862091753644088139169460760844864
+
+    for c in range(N//2):
+        step = rev_next(y)
+        x = stretch(step)
+        y = x
+
+    # print the secret which is represented as int
+    print(y.to_bytes(32, byteorder='little')[:29].decode())
