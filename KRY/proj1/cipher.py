ccf5c2168aa2a2c5314a1688c53197f62e849e6d
Petr Stehlik <petr.stehlik@kiwi.com>
2018-04-08 16:19:58 +0200

    KRY: reformat cipher.py

--- KRY/proj1/cipher.py
+++ KRY/proj1/cipher.py
@@ -7,13 +7,18 @@ SUB = [0, 1, 1, 0, 1, 0, 1, 0]
 N_B = 32
 N = 8 * N_B
 
+
 # Next keystream
 def step(x):
-  x = (x & 1) << N+1 | x << 1 | x >> N-1
-  y = 0
-  for i in range(N):
-    y |= SUB[(x >> i) & 7] << i
-  return y
+    x = (x & 1) << N+1 | x << 1 | x >> N-1
+    y = 0
+
+    for i in range(N):
+        y |= SUB[(x >> i) & 7] << i
+
+    return y
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("key")
